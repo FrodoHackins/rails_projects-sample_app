d99c7dade74ce63f01e3188696952f4a1ed972cf
Chris Kelly <cg.kelly2013@gmail.com>
2013-12-29 23:24:37 -0500

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,13 +1,33 @@
 include ApplicationHelper
 
 def valid_signin(user)
-  fill_in "Email", user.email
-  fill_in "Password", user.password
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
   click_button "Sign in"
+end
+
+def sign_in(user)
+  visit signin_path
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  cookies[:remember_token] = user.remember_token
 end
 
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
   	page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_title do |message|
+  match do |page|
+  	page.should have_selector('title', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_header do |message|
+  match do |page|
+  	page.should have_selector('h1', text: message)
   end
 end
